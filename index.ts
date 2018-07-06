0ce5b04aaf0619568419a5c81a6501aa7a87a302
eduardo.florencio <eduardo.florencio@millennium.com.br>
2018-07-06 19:22:51 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import rp from "request-promise";
+import cheerio from "cheerio";
+
+var options = {
+    uri: 'https://jovemnerd.com.br/',
+    transform: function (body: any) {
+      return cheerio.load(body);
+    }
+  };
+
+rp(options)
+    .then(($: CheerioSelector) => {
+        console.log('data');
+        var li = $('.submenu__item');
+
+        li.each((_i, element) => { 
+            var link = $(element).children('a');
+            console.log(link.attr('href'));
+        });
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    });
